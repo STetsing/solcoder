da6379f161625dfcabd94704fa48788bb33f304a
Filip Mertens <filip.mertens@ethereum.org>
2023-11-19 19:26:22 +0100

    fix cors in flask app

--- solidity_code_generation/app/main.py
+++ solidity_code_generation/app/main.py
@@ -1,4 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, [
+  "http://localhost:8080",
+  "https://remix.ethereum.org",
+  "https://remix-alpha.ethereum.org",
+  "https://remix-beta.ethereum.org",
+  "https://deploy-preview-*--remixproject.netlify.app/"
+], allow_headers="*", allow_methods="*", supports_credentials=True)
 from app.views import *
