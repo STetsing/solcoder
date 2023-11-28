ad06ff2990ce1e2ade51875089f7b74255e2a386
Filip Mertens <filip.mertens@ethereum.org>
2023-11-28 18:22:08 +0100

    fix reverse proxy issues with flask

--- solidity_code_generation/entrypoint.py
+++ solidity_code_generation/entrypoint.py
@@ -1,4 +1,8 @@
+from werkzeug.middleware.proxy_fix import ProxyFix
 from app.main import app
+
+# Make the flask app aware of the reverse proxy
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_host=1, x_port=1, x_proto=1, x_prefix=1)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, threaded=False, processes=1)
