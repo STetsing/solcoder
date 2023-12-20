ffdca3a846452d0e184041f6326642fe840cbd63
Stéphane Tetsing <stephane.fosso@yahoo.fr>
2023-12-20 09:04:05 +0100

    added model test script

---
+++ train/test_t5.py
@@ -0,0 +1,22 @@
+import torch 
+import numpy as np
+import pandas as pd
+import torch
+from datasets import Dataset
+from tqdm import tqdm
+from transformers import T5ForConditionalGeneration, RobertaTokenizer, AutoTokenizer
+device = "cuda" if torch.cuda.is_available() else 'cpu'
+
+#model_path = './trained_model_last_epoch'
+#model_path = '/Users/pippertetsing/Desktop/work/Remix/solcoder/training_models/checkpoint-720'
+model_path = 'Pipper/SolCoder'
+
+tokenizer = RobertaTokenizer.from_pretrained(model_path)
+model = T5ForConditionalGeneration.from_pretrained(model_path).to(device)
+
+def infer(comment):
+    input_ids = tokenizer(comment, return_tensors='pt').input_ids
+    outputs = model.generate(input_ids, max_new_tokens=200, temperature=0.8)
+    return tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+print(infer("contract crazy token "))
