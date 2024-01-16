fbce26bd4a3cbe073cef6dcc6427e9c791bca9fe
Stéphane Tetsing <stephane.fosso@yahoo.fr>
2024-01-16 16:26:07 +0000

    disable gradient checkpointing

--- train/train_phi2_lora.py
+++ train/train_phi2_lora.py
@@ -45,7 +45,7 @@ model = AutoModelForCausalLM.from_pretrained(base_model,
 model.config.use_cache = False
 model.config.pretraining_tp = 1
 model = prepare_model_for_kbit_training(model, use_gradient_checkpointing=True)
-model.gradient_checkpointing_enable(gradient_checkpointing_kwargs={"use_reentrant": True})
+#model.gradient_checkpointing_enable(gradient_checkpointing_kwargs={"use_reentrant": True})
 
 print('INFO: Model size is', model.num_parameters()/1e9, "GB\n")
 
