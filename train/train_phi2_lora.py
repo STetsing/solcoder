8091c1dc9889e34b2241825f607b5c476d84d0e6
Stéphane Tetsing <stephane.fosso@yahoo.fr>
2024-01-16 13:25:02 +0000

    updated layers

--- train/train_phi2_lora.py
+++ train/train_phi2_lora.py
@@ -95,7 +95,7 @@ peft_config = LoraConfig(
     lora_dropout = 0.05,
     bias = "none",
     task_type = "CAUSAL_LM",
-    target_modules = ["Wqkv", "fc1", "fc2"], #, 'q_proj', 'k_proj', 'v_proj','dense','fc1','fc2','embed_tokens','lm_head'],
+    target_modules = ["Wqkv", "fc1", "fc2", 'q_proj', 'k_proj', 'v_proj','dense','embed_tokens','lm_head'],
 )
 
 
