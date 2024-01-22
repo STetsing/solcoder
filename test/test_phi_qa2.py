1dcc1f9b8bb1a965f229898ac5fcb5ec6f675f6a
Stéphane Tetsing <stephane.fosso@yahoo.fr>
2024-01-22 10:14:44 +0000

    loaded model in 4 bit mode

--- test/test_phi_qa2.py
+++ test/test_phi_qa2.py
@@ -13,7 +13,9 @@ model_path = './Phi2-SolCoder-lora-qa2'
 
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 model = AutoModelForCausalLM.from_pretrained(model_path,
-                                            trust_remote_code=True).to(device)
+                                            load_in_4bit=True,
+                                            trust_remote_code=True,
+                                            device_map=device)
 
 print(model.get_memory_footprint()/1e9)
 model =  PeftModel.from_pretrained(model, model_path)
@@ -21,8 +23,9 @@ model = model.merge_and_unload()
 print(model.get_memory_footprint()/1e9)
 
 def infer(comment):
-    input_ids = tokenizer(comment, return_tensors='pt').to(device)#.input_ids.to(device)
+    input_ids = tokenizer(comment, return_tensors='pt')#.input_ids.to(device)
     outputs = model.generate(**input_ids, max_new_tokens=200, do_sample=True, temperature=0.8)
     return tokenizer.decode(outputs[0], skip_special_tokens=True)
 
-print(infer("### Question: Write in Solidity a function _super_adder for adding 6 uint256 numbers and return the result. The function should be internal\n### Answer:\n"))
+#print(infer("### Question: Write in Solidity a function _super_adder for adding 6 uint256 numbers and return the result. The function should be internal\n### Answer:\n"))
+print(infer("### Question: Write in Solidity a function get_winningProposal to Computes the winning proposal taking all previous votes into account.\n### Answer:\n"))
